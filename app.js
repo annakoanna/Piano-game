f2d10f0a023ca797f94baf57321471fa6a7f304d
annakoanna <anna.231991k@gmail.com>
2022-05-24 18:31:29 -0700

    created function that change text inside Start button

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 let keys = document.querySelectorAll(".key")
-let point = 0;
 let level = "Level 1";
-const start = document.getElementById('start')
 const startLevel = document.getElementById('level')
-const points = document.getElementById('button-result')
+const result = document.getElementById('button-result')
 const notes = document.getElementById('button-notes')
+let score = 0;
+const restart = document.getElementById("restart");
 
 
-keys.forEach(function(key) {
-    key.addEventListener("click", () => play(key))
+
+
+keys.forEach(function (key) {
+    key.addEventListener('click', () => play(key))
 })
 
 function play(key) {
@@ -16,22 +18,89 @@ function play(key) {
     noteAudio.currentTime = 0
     noteAudio.play()
     key.classList.add("active")
-    noteAudio.addEventListener("ended", () =>{
+    noteAudio.addEventListener("ended", () => {
         key.classList.remove("active")
+        
     })
+    console.log(key.id)
+    if(key.id == notes.innerText){
+        console.log("good job")
+        score = score + 1;
+        console.log(score);
+        let level = levelOneNotes;
+        changeText(level)
+    }
 }
-function checkNotes(arr) {
+//variable for levels
+const levelOneNotes = ["C", "E", "C", "C", "E", "C", "D", "C", "E", "E", "D", "C", "E", "C", "D"]
+const levelTwoNotes = ["E", "E", "F", "G", "G", "F", "E", "D", "C", "C", "D", "E", "E", "D", "D"] 
+const levelTreeNotes = ["G", "G", "G", "D", "E", "E", "D", "B", "B", "A", "A", "G", "D", "G", "G" ]
+const levelFourBlacknotes = ["G", "G", "G", "Df", "Ef", "Gf", "Af", "Bf", "A", "B", "Df", "Ef", "Gf", "Af", "B"]
 
+
+function changeText(arr) {
+    //for (let i = 0; i < arr.length; i++) {
+        let randomNote = Math.floor(Math.random()* arr.length)
+        notes.innerText = arr[randomNote];
+
+   // };
 }
-const levelOneNotes = ["C", "E", "C", "C", "E", "C", "D", "C", "E", "E", "D", "C"]
-const levelTwoNotes = ["E", "E", "F", "G", "G", "F", "E", "D", "C", "C", "D", "E", "E", "D", "D", "E", "E", "F", "G", "G", "F", "E", "D", "C", "C", "D", "E", "D", "C", "C" ]
-const levelTreeNotes = [ "A", "B", "A", "B", "C", ]
-const levelFourBlacknotes = [Df, Ef, Gf, Af, Bf]
-const levelfiveAllNotes  = [C, D, E, F, G, A, B, Df, Ef, Gf, Af, Bf]
-for(i = 0; i < arr.length; i ++) {
+//changing notes inside start button
+notes.addEventListener("click", (e) => {
+    if(notes.innerText == "Start"){
+        changeText(levelOneNotes);
 
-}
+    }
+   
+    //changeText(levelOneNotes);
+       
+   
+
+  
+
+    
+    
+    //setTimeout(changeText)
+    
+
+});
+/*
+document.querySelector("#restart").addEventListener('click', function(){
+    window.location.reload();
+    result = "Result:";
+    
+    
+});
 
 
-//document.querySelector("#restart").addEventListener('click', function(){
-    //window.location.reload();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
